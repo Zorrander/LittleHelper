663d178f76dd4786bef008cea19ea1f78289114b
Zorrander <alexandre.angleraud@gmail.com>
2016-12-14 09:20:58 +0000

    modif fin sprint3, carte

---
+++ test_path.py
@@ -0,0 +1,62 @@
+from Model import car
+from Controllers import preloadedPath, StmController
+#from carto import first_test2
+
+#from PyQt5.QtCore import pyqtSignal
+import time 
+
+def test():
+    model = car.Car()
+    pPath = preloadedPath.PreloadedPaths(model)
+    spi = StmController.StmController(model)
+    spi.start()
+#    pPath.start()
+
+#    map_car = first_test2.Map([100,100])
+#    display_car = first_test2.Display(map_car, model)
+#    display_car.sig.connect(display_car.a, display_car.updateFunction)
+#    display_car.sig.connect(display_car.updateFunction)
+#    t = Thread(target=display_car.triggerUpdate);
+#    t.start();
+#    display_car.w.showMaximized()
+#    sys.exit(display_car.a.exec_())
+    print("lancement du path 1")
+    pPath.start_path(0)
+
+def test2():
+    print("turn right 40")
+    model.turnRight(40)
+    time.sleep(2)
+
+    print("turn left 40")    
+    model.turnLeft(40)
+    time.sleep(2)
+    
+    print("turn left 0")    
+    model.turnLeft(0)
+    time.sleep(2)
+    
+    print("turn right 40")    
+    model.turnRight(40)
+    time.sleep(2)
+
+    print("turn left 40")    
+    model.turnLeft(40)
+    time.sleep(2)
+    
+    print("turn right 0")    
+    model.turnRight(0)
+    time.sleep(2)
+
+    print("turn right 40")    
+    model.turnRight(40)
+    time.sleep(2)
+
+    print("turn right 0")    
+    model.turnRight(0)
+    time.sleep(2)
+
+    print("turn left 0")    
+    model.turnLeft(0)
+
+test()
